6ae9ff94dead84164c8856043fb7833b53eb3905
JonathanSia-2025 <25006171@siswa-old.um.edu.my>
2025-11-11 16:23:18 +0800

    first commit

---
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication16;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MelodyCloud
+ */
+public class JavaApplication16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("how many numbers will you enter?: ");
+        int n = sc.nextInt();
+        int countO = 0, countE = 0;
+        for (int i=0; i<n; i++){
+            System.out.printf("Enter number %d ", i);
+            int number = sc.nextInt();
+            if (number % 2 == 0){
+                countE += 1;
+                
+            } else {
+                countO += 1;
+                
+            }
+        }
+        int sum_o_e = countE + countO;
+        System.out.printf("Total even numbers: %d", countE);
+        System.out.println();
+        System.out.printf("Total odd numbers: %d", countO);
+        System.out.println();
+        System.out.printf("Total sum of odd and even number %d", sum_o_e);
+    }
+    
+}
